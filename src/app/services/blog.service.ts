01e29935a3aa069a4f0b83fa23078c6bb67c7d8e
Lennart Grosser <lennartgrosser13@web.de>
2019-05-02 18:32:38 +0200

    updated blog service

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { LoginData } from '../models/loginData.model';
+import { environment } from 'src/environments/environment.prod';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,16 +14,18 @@ export class BlogService {
   userUrl: string;
 
 constructor(private http: HttpClient) { 
-  this.postUrl = "http://localhost:3000/posts";
-  this.userUrl = "http://localhost:3000/users/";
+  this.postUrl = environment.backend + environment.postsUrl;
+  this.userUrl = environment.backend + environment.usersUrl;
 }
 
 getPosts(): Observable<any> {
+  console.log(this.postUrl);
   return this.http.get<any>(this.postUrl);
 }
 
 login(loginData: LoginData){
   let url = this.userUrl + loginData.username;
+  console.log(url)
   return this.http.post<any>(url, {...loginData});
 }
 
